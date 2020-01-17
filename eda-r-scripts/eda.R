7ca9b5de1f6b42bb2adaf51f3994c0695caed58a
Connor Rothschild <connorrothschild@gmail.com>
2020-01-17 11:43:41 -0600

    start scrolling

--- eda-r-scripts/eda.R
+++ eda-r-scripts/eda.R
@@ -1,4 +1,10 @@
 library(tidyverse)
+library(ggplot2)
+library(dplyr) 
+library(viridis) 
+library(lubridate) 
+library(Interpol.T)
+library(tidyr) 
 
 cr::set_cr_theme()
 
@@ -6,14 +12,20 @@ data <- read.csv("../data/data.csv")
 data <- janitor::clean_names(data)
 
 data <- data %>% 
-  mutate(total_dead_and_missing = as.numeric(total_dead_and_missing))
+  mutate(total_dead_and_missing = as.numeric(total_dead_and_missing),
+         minimum_estimated_number_of_missing = as.numeric(minimum_estimated_number_of_missing))
+
+sum(data$total_dead_and_missing)
+sum(data$minimum_estimated_number_of_missing, na.rm = TRUE)
+
+most_deadly_incident <- max(data$minimum_estimated_number_of_missing)
 
 data %>% 
-  group_by(unsd_geographical_grouping) %>%
+  group_by(region_of_incident) %>%
   summarise(dead_and_missing = sum(total_dead_and_missing)) %>% 
-  mutate(unsd_geographical_grouping = reorder(unsd_geographical_grouping, dead_and_missing)) %>% 
+  mutate(region_of_incident = reorder(region_of_incident, dead_and_missing)) %>% 
   slice(1:10) %>% 
-  ggplot(aes(x=unsd_geographical_grouping,y=dead_and_missing)) +
+  ggplot(aes(x=region_of_incident,y=dead_and_missing)) +
   geom_col() +
   coord_flip() +
   labs(title = "Where do migrants go missing?",
@@ -30,6 +42,77 @@ data %>%
   geom_col() +
   coord_flip() +
   labs(title = "Where do migrants go missing?",
+       y = "Number dead or missing",
+       x = element_blank()) +
+  cr::fix_bars()
+
+data %>% 
+  filter(reported_year != 2020) %>% 
+  group_by(reported_year) %>%
+  summarise(dead_and_missing = sum(total_dead_and_missing)) %>% 
+  slice(1:10) %>% 
+  ggplot(aes(x=reported_year,y=dead_and_missing)) +
+  geom_col() +
+  scale_x_continuous(breaks = c(2014:2019)) +
+  labs(title = "Changes over time",
+       y = "Number dead or missing",
+       x = element_blank()) +
+  cr::fix_bars()
+
+data$reported_month <- factor(data$reported_month, levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
+
+data %>% 
+  filter(reported_year != 2020) %>% 
+  ggplot(aes(reported_month,-reported_year,fill=total_dead_and_missing)) +
+  geom_tile(color= "white",size=0.1) +  
+  scale_fill_viridis(name="Number dead\nor missing",option ="C") +
+  scale_y_continuous(breaks = c(-2014:-2019), labels = c(2014:2019)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1)) +
+  cr::drop_axis() +
+  labs(title = "When migrants die and go missing",
+       x = element_blank(), y = element_blank())
+
+data %>% 
+  group_by(cause_of_death) %>%
+  summarise(dead_and_missing = sum(total_dead_and_missing)) %>% 
+  arrange(desc(dead_and_missing)) %>%
+  mutate(cause_of_death = reorder(cause_of_death, dead_and_missing)) %>% 
+  slice(1:8) %>% 
+  ggplot(aes(x=cause_of_death,y=dead_and_missing)) +
+  geom_col() +
+  coord_flip() +
+  labs(title = "How do migrants die??",
+       y = "Number dead or missing",
+       x = element_blank()) +
+  cr::fix_bars()
+
+top_cause_of_death <- c("Drowning",                               
+                        "Sickness and lack of access to medicines",
+                        "Vehicle Accident",                        
+                        "Presumed drowning",                    
+                        "Unknown",                         
+                        "Starvation",                            
+                        "Shot",                           
+                        "Dehydration")
+top_death_countries <- c("North Africa", 
+                         "Mediterranean",
+                         "Horn of Africa",
+                         "Middle East",
+                         "Central America",
+                         "Europe",
+                         "Caribbean")
+
+data %>% 
+  filter(region_of_incident %in% top_death_countries,
+         cause_of_death %in% top_cause_of_death) %>% 
+  group_by(region_of_incident, cause_of_death) %>%
+  summarise(dead_and_missing = sum(total_dead_and_missing)) %>% 
+  ungroup() %>% 
+  ggplot(aes(x=cause_of_death,y=dead_and_missing)) +
+  geom_col() +
+  facet_wrap(~region_of_incident) + 
+  coord_flip() +
+  labs(title = "How do migrants die?",
        y = "Number dead or missing",
        x = element_blank()) +
   cr::fix_bars()
