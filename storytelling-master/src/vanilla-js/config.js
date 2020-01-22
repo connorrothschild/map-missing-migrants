5913b36fb1950a0a4625caf45810479e8188faa6
Connor Rothschild <connorrothschild@gmail.com>
2020-01-21 21:52:17 -0600

    add new stages, new images, html tags

--- storytelling-master/src/vanilla-js/config.js
+++ storytelling-master/src/vanilla-js/config.js
@@ -54,13 +54,50 @@ var config = {
                     opacity: 1
                 }
             ],
-            onChapterExit: []
+            onChapterExit: [
+                {
+                    layer: 'circles-2014',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'common-media',
+            title: 'In the public eye',
+            image: './images/alan-kurdi.jpg',
+            description: 'It was during this time of crisis that the international community became exposed to the horrific reality of these migrant deaths, often through images such as this one, which focuses on a three-year-old Syrian refugee named Alan Kurdi. <br>He and his family died in September of 2015 near Bodrum, Turkey. They were on their way to Canada.',
+            location: {
+                center: [27.246,37.009],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'circles-2014',
+                    opacity: 1
+                },
+                {
+                    layer: 'settlement-major-label',
+                    opacity: 1
+                },
+            ],
+            onChapterExit: [
+                {
+                    layer: 'settlement-major-label',
+                    opacity: 1
+                },
+                {
+                    layer: 'circles-2014',
+                    opacity: 0
+                }
+            ]
         },
         {
             id: 'circles-2019',
             // title: 'An overview',
             // image: './path/to/image/source.png',
-            description: 'The most recent complete data is from 2019. As these <span style = "color:#0010F0;">&#9673;</span> circles show, instances of dead or missing migrants are mostly concentrated in the same place. <br>Thankfully, these deaths are becoming slightly less frequent; in 2019, roughly, 3,400 migrants were found dead or missing.',
+            description: 'In the five years since the refugee crisis captured international attention, some nations have made strides to create a safer and more welcoming climate for migrants. In Canada, the intended destination for Alan Kurdi and his family, refugee policy became a key issue in their 2015 election. <br><br>As these <span style = "color:#0010F0;">&#9673;</span> circles from 2019 show, instances of dead or missing migrants are mostly concentrated in the same place. <br>Thankfully, these deaths are becoming slightly less frequent; in 2019, roughly, 3,400 migrants were found dead or missing.',
             location: {
                 center: [-10.098,45.402],
                 zoom: 1.5,
@@ -68,6 +105,10 @@ var config = {
                 bearing: 0
             },
             onChapterEnter: [
+                {
+                    layer: 'circles-2014',
+                    opacity: 1
+                },
                 {
                     layer: 'circles-2019',
                     opacity: 1
@@ -86,9 +127,9 @@ var config = {
         },
         {
             id: 'mediterranean',
-            title: 'Some paths are more dangerous than others.',
+            title: 'Some paths remain more dangerous than others',
             // image: './path/to/image/source.png',
-            description: 'The most dangerous place for migrants is the Mediterranean Sea. <br>Of the 34,688 people who have died or went missing since 2014, over 55% of them were lost in the Mediterranean.',
+            description: 'After five years of this crisis and tepid responses from national governments, some paths remain more dangerous than others. The most dangerous place for migrants is the Mediterranean Sea, where Alan Kurdi drowned.<br><br>Of the 34,688 people who have died or went missing since 2014, over 55% of them were lost in the Mediterranean.',
             location: {
                 center: [14.350,30.65],
                 zoom: 3,
@@ -120,7 +161,7 @@ var config = {
             id: 'mediterranean-focus',
             title: 'The deadliest single incident in the last six years',
             image: './images/two-traffickers.jpg',
-            description: 'On April 18, 2015, two human traffickers intentionally capsized a migrant boat somewhere near the coast of Libya. 750 migrants died of drowning and an additional 252 went missing somewhere between Lampedusa and Libya.<br>In total, 1,022 migrants died or went missing—only 27 survived.',
+            description: 'This region also includes some of the most horrendous, intentional incidents. On April 18, 2015, two human traffickers intentionally capsized a migrant boat somewhere near the coast of Libya. 750 migrants died of drowning and an additional 252 went missing.<br><br>In total, 1,022 migrants died or went missing—27 survived.',
             location: {
                 center: [12.350,34.65],
                 zoom: 5,
@@ -165,8 +206,56 @@ var config = {
             ]
         },
         {
+            id: 'italy-focus',
+            title: 'A deadly trend',
+            // image: './images/two-traffickers.jpg',
+            description: 'This incident had been one of many. Seven months prior to the Libyan shipwreck, human traffickers had intentionally sunk another boat with over 500 Syrian, Palestinian, Egyptian, and Sudanese migrants.',
+            location: {
+                center: [12.350,34.65],
+                zoom: 5,
+                pitch: 0,
+                bearing: 0
+        },
+            onChapterEnter: [
+                {
+                    layer: 'country-label',
+                    opacity: 1
+                },
+                {   
+                    layer: 'overview-circles',
+                    opacity: .3
+                },
+                {
+                    layer: 'italy-focus',
+                    opacity: .9
+                },
+                {
+                    layer: 'italy-text',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'country-label',
+                    opacity: 0
+                },
+                {   
+                    layer: 'overview-circles',
+                    opacity: .51
+                },                
+                {
+                    layer: 'deadliest-incident-text',
+                    opacity: 0
+                },
+                {
+                    layer: 'deadliest-incident-focus',
+                    opacity: 0
+                }
+            ]
+        },
+        {
             id: 'us-mexico',
-            title: 'Closer to Home',
+            title: 'Closer to home',
             // image: './path/to/image/source.png',
             description: 'A large number of migrants have also died near the US-Mexico border. Since 2014, over 2,400 migrants have died or gone missing in this area, making it the third most dangerous region in the world for migrants. It is only surpassed by the Mediterranean and North Africa.',
             location: {
@@ -253,7 +342,7 @@ var config = {
             id: 'us-mexico-tucson',
             title: 'Southern Arizona',
             // image: './path/to/image/source.png',
-            description: "One of these areas is southern Arizona. Cities such as Tucson and Sierra Vista witness much migration from the US-Mexico border, and much of it leads to treacherous outcomes. Independent NGOs have <a target = '_blank' href = 'https://humaneborders.org/wp-content/uploads/deathmapcumulative_poster_2018.pdf'>estimated that</a>, since 1999, over 3300 migrants have died while crossing the Arizona border. <br>The largest concentration is in Pima County. In 2018 alone, the remains of 127 migrants were <a target= '_blank' href = 'https://www.azcentral.com/story/news/politics/border-issues/2019/01/16/remains-127-dead-migrants-recovered-southern-arizona-2018/2575080002/'>given to the Pima County Medical Examiner's Office</a>.",
+            description: "One of these areas is southern Arizona. Cities such as Tucson and Sierra Vista witness much migration from the US-Mexico border, and much of it leads to treacherous outcomes. Independent NGOs have <a target = '_blank' href = 'https://humaneborders.org/wp-content/uploads/deathmapcumulative_poster_2018.pdf'>estimated that</a>, since 1999, over 3300 migrants have died while crossing the Arizona border. <br><br>The largest concentration is in Pima County. In 2018 alone, the remains of 127 migrants were <a target= '_blank' href = 'https://www.azcentral.com/story/news/politics/border-issues/2019/01/16/remains-127-dead-migrants-recovered-southern-arizona-2018/2575080002/'>given to the Pima County Medical Examiner's Office</a>.",
             location: {
                 center: [-113.439, 32.031],
                 zoom: 6,
@@ -293,7 +382,7 @@ var config = {
             id: 'us-mexico-brooks-county',
             title: 'Brooks County',
             // image: './path/to/image/source.png',
-            description: "The other area of focus is the Southeastern part of Texas. There are deaths scattered throughout, but the greatest concentration is in one place: Brooks County. <br>Over <a target = '_blank' href = 'https://www.nytimes.com/2019/04/18/us/mexico-border-deaths.html'>640 migrants' remains</a> have been found in this county since 2009.",
+            description: "The other area of focus is the Southeastern part of Texas. There are deaths scattered throughout, but the greatest concentration is in one place: Brooks County. <br><br>Over <a target = '_blank' href = 'https://www.nytimes.com/2019/04/18/us/mexico-border-deaths.html'>640 migrants' remains</a> have been found in this county since 2009.",
             location: {
                 center: [-99.530, 26.919],
                 zoom: 6.23,
@@ -437,7 +526,7 @@ var config = {
             id: 'final',
             title: 'Since 2014, over 34,000 migrants have died or went missing on their journey to a better life.',
             // image: './path/to/image/source.png',
-            description: "<p>What can you do to help?<br>The following organizations fight for immigrants&#39; rights in the U.S. and abroad:<br></p><ul><li><a target = '_blank' href='https://asylumadvocacy.org/'>Asylum Seeker Advocacy Project</a></li><li><a target = '_blank' href='https://www.raicestexas.org/'>RAICES</a></li><li><a target = '_blank' href='https://las-americas.org/'>Las Americas Immigrant Advocacy Center</a></li><li><a target = '_blank' href='https://www.refugeesinternational.org/'>Refugees International</a></li></ul>",
+            description: "<p>What can you do to help?<br>The following organizations fight for immigrant safety in the U.S. and abroad:<br></p><ul><li><a target = '_blank' href='https://asylumadvocacy.org/'>Asylum Seeker Advocacy Project</a></li><li><a target = '_blank' href='https://www.raicestexas.org/'>RAICES</a></li><li><a target = '_blank' href='https://las-americas.org/'>Las Americas Immigrant Advocacy Center</a></li><li><a target = '_blank' href='https://www.refugeesinternational.org/'>Refugees International</a></li></ul>",
             location: {
                 center: [-24.094,2.358],
                 zoom: 1.5,
